197fec8ac3147e555b0832f9fc5c5601e8b967df
masihgh <masihprogrammer@gmail.com>
2023-03-01 21:21:25 +0330

    change port to 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 3000
+const port = 80
 let data = require('./data.json')
 app.use(cors({
   origin: '*'
